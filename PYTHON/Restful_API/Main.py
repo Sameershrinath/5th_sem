8787645b3b684e43e595a94fc0aaa4381004782d
Sameer shrinath <sameershrinath@gmail.com>
2025-06-27 23:46:30 +0530

    27.06.25

--- PYTHON/Restful_API/Main.py
+++ PYTHON/Restful_API/Main.py
@@ -15,3 +15,29 @@ class Model_input(BaseModel):
     BMI:float
     DiabetesPedigreeFunction:float
     Age:int
+
+diabetes_model=pickle.load(open("diabetes_model.sav","rb"))
+
+@app.post('/diabetes_prediction')
+def diabetes_pred(input_parameters:Model_input):
+    input_data=input_parameters.json()
+    input_dictionary=json.loads(input_data)
+
+    preg=input_dictionary['Pregnancies']
+    glu=input_dictionary['Glucose']
+    bp=input_dictionary['BloodPressure']
+    skin=input_dictionary['SkinThickness']
+    insulin=input_dictionary['Insulin']
+    bmi=input_dictionary['BMI']
+    dpf=input_dictionary['DiabetesPedigreeFunction']
+    age=input_dictionary['Age']
+
+    input_list=[preg,glu,bp,skin,insulin,bmi,dpf,age]
+
+    result=diabetes_model.predict([input_list])
+
+    if result[0]==0:
+        return 'the person is not diabetic'
+    else:
+        return 'the person is diabetic'
+
