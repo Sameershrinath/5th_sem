8b9be957ce7f2afcea636e0547798b11348674e7
Sameer shrinath <sameershrinath@gmail.com>
2025-10-14 10:12:55 +0530

    update

--- Web/Node_learning/calculatorProj/sum.js
+++ Web/Node_learning/calculatorProj/sum.js
@@ -1,14 +1,35 @@
 const sumRequesthandler=(req,res)=>{
-console.log("in sum request handler")
+
 const body=[];
-req.on('data',chunk=>{body.push(chunk)});
+req.on('data',chunk=>{
+    body.push(chunk);
+    console.log(chunk);
+});
 req.on('end',()=>{
+    console.log(`THis is body : ${body} ${typeof(body)}`);
+
+
     const bodystr=Buffer.concat(body).toString();
+    console.log(`THis is bufferrstr : ${bodystr} ${typeof(bodystr)}`);
+
+
     const params=new URLSearchParams(bodystr);
+    console.log(`THis is params : ${params} ${typeof(para)}`);
+
+
     const bodyObj=Object.fromEntries(params);
+    console.log(`THis is bodyObj : ${bodyObj}`);
+
+
     const result=Number(bodyObj.first)+Number(bodyObj.second);
+
     res.setHeader('Content-Type','text/html');
-    res.write(`<html><title>${result}</title><body>The response is ${result}</body></html>`)
+    res.write(`<html>
+        <title>${result}</title>
+        <body>
+        <h1>The response is ${result}</h1>
+        </body>
+        </html>`)
 })
 }
 module.exports=sumRequesthandler;
