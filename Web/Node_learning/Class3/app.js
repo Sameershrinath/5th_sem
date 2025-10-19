71470fd109d3a4d198e262a52f78b6776f4c8ad0
Sameer shrinath <sameershrinath@gmail.com>
2025-10-19 13:01:55 +0530

    code oon the 19.10.2025

---
+++ Web/Node_learning/Class3/app.js
@@ -0,0 +1,14 @@
+const fs= require('fs');
+console.log('1. start of the script');
+
+console.log('2. reading file sunchronously');
+const dataSync=fs.readFileSync('user-detail.txt','utf8');
+console.log('3. Synchronous read complete');
+
+console.log('4. Reading the file asynchronouslly');
+fs.readFile('user-detail.txt','utf8',(err,dataAsync)=>{
+    if (err) throw err;
+    console.log('6. Asynchronous read complete');
+});
+
+console.log('5.End of script');
